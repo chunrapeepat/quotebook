bde5789f96194fe7b6f5e6ce1616e9439ebaf3fd
chunza2542 <chunza2542@hotmail.com>
2018-06-06 21:40:53 +0700

    Fix #40, integrate with mongodb (docker)

---
+++ config/database.js
@@ -0,0 +1,23 @@
+// database username (admin)
+const username = exports.username = 'quotebook'
+
+// database password (secure)
+const password = exports.password = 'password'
+
+// database port
+const port = exports.port = '2277'
+
+// database name
+const name = exports.name = 'quotebook'
+
+// database host
+const host = exports.host = 'localhost'
+
+// mongodb connection url
+exports.mongourl = `mongodb://${username}:${password}@${host}:${port}/${name}`
+
+// mongodb setting options
+exports.options = {
+  autoReconnect: true,
+  authMechanism: 'SCRAM-SHA-1',
+}
--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const https = require('https')
 const next = require('next')
 const path = require('path')
 const fs = require('fs')
-const appConfig = require('./config/app')
+const mongoose = require('mongoose')
 
+const appConfig = require('./config/app')
+const dbConfig = require('./config/database')
 const apiHandler = require('./handlers')
 
 const dev = appConfig.environment !== 'production'
@@ -16,6 +18,13 @@ const certOptions = {
   key: fs.readFileSync(path.resolve('server.key')),
   cert: fs.readFileSync(path.resolve('server.crt')),
 }
+
+// connect to mongo database
+mongoose.connect(dbConfig.mongourl, dbConfig.options)
+  .then(
+    () => console.log(`mongodb has beed connected.`),
+    err => { throw err }
+  )
 
 app.prepare()
   .then(() => {
@@ -32,10 +41,7 @@ app.prepare()
       return handle(req, res)
     })
 
-    // server.listen(appConfig.port, err => {
-    //   if (err) throw err
-    //   console.log(`${appConfig.name} v${appConfig.version} listening on port ${appConfig.port}`)
-    // })
-
-    https.createServer(certOptions, server).listen(appConfig.port)
+    https.createServer(certOptions, server).listen(appConfig.port, () => {
+      console.log(`${appConfig.name} v${appConfig.version} listening on port ${appConfig.port}`)
+    })
   })
