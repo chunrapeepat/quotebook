b080e6b334d6728ef4691d09033c5d0de98fd6c2
chunza2542 <chunza2542@hotmail.com>
2018-06-07 15:17:17 +0700

    refactor /auth/facebook using async await

--- handlers/auth.js
+++ handlers/auth.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
   res.redirect(baseURL)
 })
 
-router.get('/facebook', (req, res) => {
+router.get('/facebook', async (req, res) => {
   const code = req.query.code
   // if code is empty return error
   if (code == null) {
@@ -21,45 +21,34 @@ router.get('/facebook', (req, res) => {
     })
   }
   // request access token
-  axios.get(`https://graph.facebook.com/oauth/access_token?client_id=${fbConfig.appID}&redirect_uri=${fbConfig.callbackURI}&client_secret=${fbConfig.appSecret}&code=${code}`)
-    .then(response => {
-      const accessToken = response.data.access_token
-      axios.get(`https://graph.facebook.com/v3.0/me?fields=id%2Cname%2Cemail&access_token=${accessToken}`)
-        .then(response => {
-          const profile = response.data
-          // if user not registered, add user to database.
-          userAPI.newUserRegister(profile)
-          // sign jwt token expired in 3 hours
-          const token = jwt.sign({
-            id: profile.id,
-          }, jwtConfig.secret, { expiresIn: '3h' })
-          // update token on database
-          userAPI.updateToken(profile.id, token)
-          // response token
-          return res.json({
-            success: true,
-            payload: {
-              token,
-            }
-          })
-        })
-        .catch(err => {
-          // bad request
-          return res.json({
-            error: true,
-            message: 'invalid verification code.'
-          })
-        })
-    })
-    .catch(err => {
-      // bad request
-      if (err.response.status == 400) {
-        return res.json({
-          error: true,
-          message: 'invalid verification code.'
-        })
+  try {
+    const accessToken = await axios.get(`https://graph.facebook.com/oauth/access_token?client_id=${fbConfig.appID}&redirect_uri=${fbConfig.callbackURI}&client_secret=${fbConfig.appSecret}&code=${code}`)
+      .then(res => res.data.access_token)
+    // request for user profile
+    const profile = await axios.get(`https://graph.facebook.com/v3.0/me?fields=id%2Cname%2Cemail&access_token=${accessToken}`)
+      .then(res => res.data)
+    // if user not registered, add user to database.
+    userAPI.newUserRegister(profile)
+    // sign jwt token expired in 3 hours
+    const token = jwt.sign({
+      id: profile.id,
+    }, jwtConfig.secret, { expiresIn: '3h' })
+    // update token on database
+    userAPI.updateToken(profile.id, token)
+    // response token
+    return res.json({
+      success: true,
+      payload: {
+        token,
       }
     })
+  } catch (e) {
+    console.error('err', e)
+    return res.json({
+      error: true,
+      message: 'invalid verification code.'
+    })
+  }
 })
 
 module.exports = router
